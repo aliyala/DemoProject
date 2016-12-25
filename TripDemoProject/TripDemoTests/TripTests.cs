d63f255a9f3a4a42fe8c8b4ae60965de6b1d3277
aliyala <aliya2406@yandex.ru>
2016-12-25 19:13:06 +0300

    add test project, test class

---
+++ TripDemoProject/TripDemoTests/TripTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TripBuilding;
+
+namespace TripBuildingUnitTest
+{
+    [TestClass]
+    public class TripTests
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            //arrange
+            var intervals = new List<Interval>();
+            intervals.Add(new Interval { From = "Уфа", To = "Чита" });
+            intervals.Add(new Interval { From = "Казань", To = "Москва" });
+            intervals.Add(new Interval { From = "Нижний Новгород", To = "Уфа" });
+            intervals.Add(new Interval { From = "Санкт-Петербург", To = "Нижний Новгород" });
+            intervals.Add(new Interval { From = "Москва", To = "Санкт-Петербург" });
+
+            var trip = new Trip(intervals);
+
+            //act
+            trip.Sort();
+
+            //assert
+            var list = trip.Intervals.ToList();
+            if (list.Count > 1)
+            {
+                for (var i = 0; i < list.Count - 1; i++)
+                {
+                    Assert.AreEqual(list[i].To, list[i + 1].From,
+                        "Пункт назначения на карточке {0} \"{1}\" не совпадает с пунктом отправления на следующей карточке \"{2}\"", i, list[i].To, list[i + 1].From);
+                }
+            }
+        }
+    }
+}
